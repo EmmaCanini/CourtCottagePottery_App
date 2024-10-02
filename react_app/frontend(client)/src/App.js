db0c0f7761712fc6fec9ac3143f65574ef2291cb
Emma Canini <emma.canini@btconnect.com>
2024-10-02 17:45:11 +0100

    Styling of main/homepage started

--- react_app/frontend(client)/src/App.js
+++ react_app/frontend(client)/src/App.js
@@ -1,6 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ProductList from './Components/ProductList';
-// import image from './Assets/Logo.webp';
+import image from './Assets/banner_pic.jpg';
+import logo from './Assets/logo.jpg';
+import './CSS/App.css';
 
 
 
@@ -8,14 +10,30 @@ function App() {
   return (              
     <div className="App">  {/*/this div acts as a container for the whole app (className for CSS referencing) */}
     
-      <div className="header-panel">            
-          {/*<img className="logo" src={image} alt="Logo" />  */}
-          <h1 className= "title">All Products</h1>
-          </div>
+    
+          
+    
+    <div className="panel">
+  <div className="top-bar">
+    <h1 className="banner-title">Court Cottage Pottery</h1>
+    <img className="logo" src={logo} alt= "Logo" />
+    <div className="buttons">
+    <button className="shop-button">Shop</button>
+    <button className="about-button">About</button>
+    <button className="contact-button">Contact</button>
+    </div>
+  </div>
+</div>
+
+          <img className="main-image" src={image} alt="Main_image"  /> 
+
+
+          <h2 className= "title">Featured Products</h2>
+          
  
 
       <div className="content-container">     {/*// div for Productlist container */}
-        <ProductList />
+        <ProductList /> 
       </div>
 
     </div>
